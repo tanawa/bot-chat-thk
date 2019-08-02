abde420d76bf8a58e0e71e3efb259763784885f9
tanawa <tempkakao@gmail.com>
2019-08-02 08:56:34 +0700

    Update bot-chat.js

--- bot-chat.js
+++ bot-chat.js
@@ -12,8 +12,10 @@ function processing(){
 	let text=document.getElementById("input").value;
 	if(data[text]!=undefined){
 		let system=sys+data[text];
+		console.log(system);
 		document.getElementById("output").innerHTML=system;
 	}else{
+		console.log("ไม่รู้เรื่องเลย ช่วยสอนหน่อย");
 		document.getElementById("output").innerHTML="ไม่รู้เรื่องเลย ช่วยสอนหน่อย";
 	}
 }
